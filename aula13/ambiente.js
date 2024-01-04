2ed9e8310e91448fcf9c94a32414672c71d3e36c
Caio-iron-monkey <caio-angel@hotmail.com>
2024-01-04 21:28:18 +0000

    Estudos de estruturas de repetição

---
+++ aula13/ambiente.js
@@ -0,0 +1,28 @@
+// Estrutura while, Teste logico no inicio
+
+var c = 1
+    while (c <= 6){
+       console.log(`Passo${c}`)
+        c++  //c = c + 1
+    }
+// Estrutura do while, Teste logico no fim
+var c = 1
+do {
+    console.log(`Passo${c}`)
+    c++
+}while (c <= 6)
+
+
+
+
+
+
+
+/*
+console.log('Tudo bem ?')
+console.log('Tudo bem ?')
+console.log('Tudo bem ?')
+console.log('Tudo bem ?')
+console.log('Tudo bem ?')
+console.log('Tudo bem ?')
+*/
